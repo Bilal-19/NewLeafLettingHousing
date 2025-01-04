6717cbf0e87dccfa13c67f6951c706f80e2b1f77
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-04 13:08:40 +0500

    Created Admin Dashboard.

---
+++ resources/views/AdminLayout/Dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('Admin.DashboardTemplate')
+@section('main-section')
+    <div class="container-fluid">
+        <div class="row mt-3">
+            <h3 class="fw-bold text-center">Admin Dashboard</h3>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TenantController;
 use Illuminate\Support\Facades\Route;
@@ -38,3 +39,7 @@ Route::get("/landlords", [TenantController::class, 'Landlords'])->name('Landlord
 Route::get("/faqs", [TenantController::class, 'FAQs'])->name('FAQs');
 Route::get("/contact", [TenantController::class, 'Contact'])->name('Contact');
 Route::get("/properties", [TenantController::class, 'Properties'])->name('Properties');
+
+
+// Admin
+Route::get("/admin", [AdminController::class, 'Dashboard'])->name('Dashboard');
