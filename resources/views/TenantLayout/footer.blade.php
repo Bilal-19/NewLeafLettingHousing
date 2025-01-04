6cd8c6a64d2bc41008456ec856fdd33abc5d3eea
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-04 11:13:22 +0500

    Updated footer.

--- resources/views/TenantLayout/footer.blade.php
+++ resources/views/TenantLayout/footer.blade.php
@@ -4,13 +4,19 @@
             <div class="row d-flex justify-content-between">
                 <div class="col-md-4">
                     <img src="{{ asset('images/Logo.png') }}" alt="Green Key Logo" class="img-fluid footer-logo">
-                    <h5 class="fw-semibold fs-32">Take the First Step</h5>
-                    <p class="fs-20">Let us help you turn your property into a powerful force for good.</p>
+                    <h5 class="fw-semibold fs-32 fs-sm-25">Take the First Step</h5>
+                    <p class="fs-20 fs-sm-18">Let us help you turn your property into a powerful force for good.</p>
                 </div>
                 <div class="col-md-3">
-                    <p class="fs-20 fw-semibold">Quick Links</p>
-                    <a href="" class="d-block text-light text-decoration-none fs-18">Book a Free Consultation</a>
-                    <a href="" class="text-light text-decoration-none fs-18">Learn More About Us</a>
+                    <p class="fs-20 fw-semibold fs-sm-18">Quick Links</p>
+                    <a href="" class="d-block text-light text-decoration-none fs-18 fs-sm-16">
+                        Book a Free Consultation
+                        <img src="{{ asset('images/rightArrowSm.png') }}" alt="small right arrow">
+                    </a>
+                    <a href="" class="text-light text-decoration-none fs-18 fs-sm-16">
+                        Learn More About Us
+                        <img src="{{ asset('images/rightArrowSm.png') }}" alt="small right arrow">
+                    </a>
                 </div>
             </div>
         </div>
@@ -23,7 +29,8 @@
     </div>
 
     <div class="row text-center">
-        <p class="fs-18 fs-sm-16">Copyright &copy; 2024 New Leaf Housing. <span class="text-decoration-underline fw-medium">All Rights Reserved.</span></p>
+        <p class="fs-18 fs-sm-16">Copyright &copy; 2024 New Leaf Housing. <span
+                class="text-decoration-underline fw-medium">All Rights Reserved.</span></p>
     </div>
 </div>
 
