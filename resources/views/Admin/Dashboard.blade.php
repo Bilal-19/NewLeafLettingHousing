e934e10e1c05edfcdb5df67e04124171b524fefb
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-01-14 15:00:14 +0500

    Updated admin dashboard.

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -2,7 +2,11 @@
 @section('main-section')
     <div class="container-fluid">
         <div class="row mt-3">
-            <h3 class="fw-bold text-center">Admin Dashboard</h3>
+            <h3 class="fw-bold text-center">Admin <span class="text-forest-green">Dashboard</span></h3>
+        </div>
+
+        <div class="row">
+            <p>Welcome <span class="text-forest-green">{{Auth::user()->name}}</span></p>
         </div>
     </div>
 @endsection
